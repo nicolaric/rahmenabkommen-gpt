ce97d9f53f6c6ca0e193898bd1d18305a837414d
Michael Gasche <autumo.switzerland@gmail.com>
2025-06-28 20:44:06 +0200

    UI Fixes
    - Header war bei conversations ausgeblendet
    - Titel hatte eine falsche Abmessung bei, verkleinern des Bildschirms
    - Die zu vielen und alten Links waren noch in den conversations
    - Beim Help oben ein bisschen mehr Abstand zum text, harmonischer und passt mit den anderen Seiten
    - api/bin/activate.sh fix

--- ui/app/routes/conversations.$id.tsx
+++ ui/app/routes/conversations.$id.tsx
@@ -34,35 +34,12 @@ export default function ConversationDetails() {
         <div className="min-h-screen w-full">
             {/* Hintergrundbild am unteren Rand, volle Breite, hinter allem */}
             {/*<div
-          className="fixed bottom-0 left-0 w-full h-[60rem] bg-no-repeat bg-bottom bg-cover opacity-35 dark:opacity-15"
-          style={{ backgroundImage: `url('/matter-back.webp')` }}
-          aria-hidden="true"
-      />*/}
-            <Header>
-                <a
-                    href="https://github.com/nicolaric/rahmenabkommen-gpt"
-                    target="_blank"
-                    className="hidden rounded-full p-2 hover:bg-neutral-300 dark:bg-gray-800 dark:hover:bg-gray-700 sm:block"
-                >
-                    <img
-                        src="/github-mark.svg"
-                        alt="GitHub Logo"
-                        className="h-6 w-6 transition hover:brightness-125 dark:invert"
-                    />
-                </a>
-                <a
-                    href="https://x.com/NicolaRic2"
-                    target="_blank"
-                    className="hidden rounded-full p-2 hover:bg-neutral-300 dark:bg-gray-800 dark:hover:bg-gray-700 sm:block"
-                >
-                    <img
-                        src="/X.svg"
-                        alt="X Logo"
-                        className="h-6 w-6 transition hover:brightness-125 dark:invert"
-                    />
-                </a>
-            </Header>
-            <div className="flex h-full w-full flex-col items-center justify-center">
+                className="fixed bottom-0 left-0 w-full h-[60rem] bg-no-repeat bg-bottom bg-cover opacity-35 dark:opacity-15"
+                style={{ backgroundImage: `url('/matter-back.webp')` }}
+                aria-hidden="true"
+            />*/}
+            <Header hideLogo={false} />
+            <div className="flex h-full w-full flex-col pt-4 items-center justify-center">
                 <div className="flex w-11/12 max-w-[48rem] flex-col">
                     <div className="flex flex-col gap-6 space-y-4 pb-16 pt-20">
                         <Conversation messages={conversation.messages} />
